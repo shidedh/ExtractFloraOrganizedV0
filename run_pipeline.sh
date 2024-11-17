67ec69bd32c15505a0d575e510678585566f5b14
Shide Dehghani <shidedehghani@berkeley.edu>
2024-11-17 08:52:49 -0800

    script with commands to run the pipeline and time everything and extract packages -- more instruction inside the file. Note timing the entire pipeline is not working currently

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -13,8 +13,7 @@ grep -rE "^\s*(import|from)\s+" "$search_dir" | awk '{print $2}' | sort | uniq |
 
 
 # Measure the total time from start to end
-{ time 
-    (
+{ time ( 
     cd ./preprocessing
     echo "Preprocessing is running..." | tee -a "$log_file"
     { time python3.10 get_char_df.py ; } 2>&1 | tee  -a "$log_file"
@@ -31,7 +30,7 @@ grep -rE "^\s*(import|from)\s+" "$search_dir" | awk '{print $2}' | sort | uniq |
     cd ./main_text
     echo "Main text is running..." | tee -a "$log_file"
     echo "1) find_entry_boxes.py" | tee -a "$log_file"
-    { time python3.10 find_entry_boxes.py.py ; } 2>&1 | tee -a "$log_file"
+    { time python3.10 find_entry_boxes.py ; } 2>&1 | tee -a "$log_file"
     echo "find_entry_boxes.py completed. Log file created at: $log_file" | tee -a "$log_file"
     echo "2) entry_bbox_page_cont.py" | tee -a "$log_file"
     { time python3.10 entry_bbox_page_cont.py ; } 2>&1 | tee -a "$log_file"
@@ -39,9 +38,7 @@ grep -rE "^\s*(import|from)\s+" "$search_dir" | awk '{print $2}' | sort | uniq |
     echo "3) parsing_locations.py" | tee -a "$log_file"
     { time python3.10 parsing_locations.py ; } 2>&1 | tee -a "$log_file"
     echo "parsing_locations.py completed. Log file created at: $log_file" | tee -a "$log_file"
-    cd .. 
-    ) 
-} 2>&1 | tee -a "$log_file"
+    cd .. ) } 2>&1 | tee -a "$log_file"
 
 # run chmod +x run_pipeline.sh to make this file executable 
 # execute with ./run_pipeline.sh from the root directory
